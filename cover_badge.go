23eb0e2552fb4579588c97e76c50c28726a4a274
Ivan Tanasković <startupit69@gmail.com>
2016-01-23 11:56:05 +0100

    experiment with demo gif badge

---
+++ cover_badge.go
@@ -0,0 +1,52 @@
+// Code coverage animated gif
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+)
+
+var palette = []color.Color{color.Black, color.RGBA{0xff, 0x00, 0x00, 0xff}, color.RGBA{0x00, 0xff, 0x00, 0xff}, color.White}
+
+const (
+	blackIndex = 0
+	redIndex   = 1
+	greenIndex = 2
+	whiteIndex = 3
+)
+
+func cover_badge(out io.Writer, percent int) {
+	const (
+		bedgeLength        = 50
+		bedgeHeight        = 25
+		numberOfFrames     = 100
+		delayBetweenFrames = 5
+		lastFrameDelay     = 30
+	)
+
+	anim := gif.GIF{LoopCount: numberOfFrames}
+
+	for i := 0; i < numberOfFrames; i++ {
+		rect := image.Rect(0, 0, bedgeLength, bedgeHeight)
+		img := image.NewPaletted(rect, palette)
+
+		for verticalPosition := 0; verticalPosition < bedgeHeight; verticalPosition++ {
+			for horisontalPosition := 0; horisontalPosition < bedgeLength; horisontalPosition++ {
+				img.SetColorIndex(horisontalPosition, verticalPosition, greenIndex)
+			}
+		}
+
+		switch {
+		case i == numberOfFrames-1:
+			anim.Delay = append(anim.Delay, lastFrameDelay)
+		default:
+			anim.Delay = append(anim.Delay, delayBetweenFrames)
+		}
+
+		anim.Image = append(anim.Image, img)
+	}
+
+	gif.EncodeAll(out, &anim)
+}
--- go_cover_reporter.go
+++ go_cover_reporter.go
@@ -10,6 +10,9 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler) // each request calls handler function
+	http.HandleFunc("/demo_badge", func(w http.ResponseWriter, r *http.Request) {
+		cover_badge(w, 0)
+	})
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
 
