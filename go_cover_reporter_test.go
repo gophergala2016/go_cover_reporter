34c7116277cd0b6ef42cb7088989ccb6acab558c
Ivan Tanasković <startupit69@gmail.com>
2016-01-24 21:09:53 +0100

    added two more tests

--- go_cover_reporter_test.go
+++ go_cover_reporter_test.go
@@ -8,3 +8,25 @@ func TestDummyFunction(t *testing.T) {
 		t.Errorf("dummyFunction(%d, %d) = %d, want %d", one, two, x, three)
 	}
 }
+
+func TestToTextFunction0(t *testing.T) {
+	const (
+		frameNumber    = 40
+		percentage     = 0.0
+		expectedString = "cover:0.00%"
+	)
+	if generatedString := toText(frameNumber, percentage); generatedString != expectedString {
+		t.Errorf("toText(%d, %.2f) = %s, want %s", frameNumber, percentage, generatedString, expectedString)
+	}
+}
+
+func TestToTextFunction333(t *testing.T) {
+	const (
+		frameNumber    = 40
+		percentage     = 33.3
+		expectedString = "cover:33.30%"
+	)
+	if generatedString := toText(frameNumber, percentage); generatedString != expectedString {
+		t.Errorf("toText(%d, %.2f) = %s, want %s", frameNumber, percentage, generatedString, expectedString)
+	}
+}
