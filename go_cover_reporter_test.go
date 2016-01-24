2ea19d5bb0181fc6482bfa41dd9452fe2ea6f20f
Ivan Tanasković <startupit69@gmail.com>
2016-01-24 22:04:33 +0100

    added  more tests

--- go_cover_reporter_test.go
+++ go_cover_reporter_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func TestDummyFunction(t *testing.T) {
 	const one, two, three = 1, 2, 3
@@ -16,7 +21,7 @@ func TestToTextFunction0(t *testing.T) {
 		expectedString = "cover:0.00%"
 	)
 	if generatedString := toText(frameNumber, percentage); generatedString != expectedString {
-		t.Errorf("toText(%d, %.2f) = %s, want %s", frameNumber, percentage, generatedString, expectedString)
+		t.Errorf("toText(%d, %.2f) = %s, expected %s", frameNumber, percentage, generatedString, expectedString)
 	}
 }
 
@@ -27,6 +32,27 @@ func TestToTextFunction333(t *testing.T) {
 		expectedString = "cover:33.30%"
 	)
 	if generatedString := toText(frameNumber, percentage); generatedString != expectedString {
-		t.Errorf("toText(%d, %.2f) = %s, want %s", frameNumber, percentage, generatedString, expectedString)
+		t.Errorf("toText(%d, %.2f) = %s, expected %s", frameNumber, percentage, generatedString, expectedString)
 	}
+}
+
+func TestCoverBadgeFunction(t *testing.T) {
+
+	file, err := ioutil.TempFile(os.TempDir(), "tmp_test_dir")
+	if err != nil {
+		t.Error("Unable to create temp directory.")
+	}
+	defer os.Remove(file.Name())
+
+	coverBadge(io.Writer(file), 12.3)
+
+	fileinfo, err := file.Stat()
+	if err != nil {
+		t.Error("No gif image file was created.")
+	}
+
+	if fileinfo.Size() < 20000 {
+		t.Error("File is significantly smaller than expected.")
+	}
+
 }
